2503c2e4cdd48dda13a90c463e717c129d0474d1
DESKTOP-V0E8EJ1\alexs <alexshockency@gmail.com>
2021-02-20 05:10:09 -0700

    Retain window size inbetween runs

--- 5e_init_tracker/Electron/main.js
+++ 5e_init_tracker/Electron/main.js
@@ -1,22 +1,48 @@
-const { app, BrowserWindow } = require('electron');
-const isDev = require('electron-is-dev');   
-const path = require('path');
- 
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
+const Store = require('./store.js');
+
 let mainWindow;
- 
+
+const store = new Store({
+    // We'll call our data file 'user-preferences'
+    configName: 'user-preferences',
+    defaults: {
+      // 800x600 is the default size of our window
+      windowBounds: { width: 800, height: 600 }
+    }
+  });
+
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width:800,
-        height:600,
-        show: false
-    });
-    const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
- 
-    mainWindow.loadURL(startURL);
-    //mainWindow.removeMenu();
-    mainWindow.once('ready-to-show', () => mainWindow.show());
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+  let { width, height } = store.get('windowBounds');
+  let maximized = store.get('maximized')
+
+  mainWindow = new BrowserWindow({ width, height,show: false });
+
+  if(maximized){
+      mainWindow.maximize();
+  }
+
+  mainWindow.on('resize', () => {
+    let { width, height } = mainWindow.getBounds();
+    store.set('windowBounds', { width, height });
+  });
+
+  mainWindow.on('maximize', () => {
+    let  maximized = true;
+    store.set('maximized',  maximized );
+  });
+
+  const startURL = isDev
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "../build/index.html")}`;
+
+  mainWindow.loadURL(startURL);
+  //mainWindow.removeMenu();
+  mainWindow.once("ready-to-show", () => mainWindow.show());
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
-app.on('ready', createWindow);
+app.on("ready", createWindow);
