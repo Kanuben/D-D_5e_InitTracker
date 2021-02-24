8fac71990aa375e0a1e3c05d6bb4f44be5534f80
DESKTOP-V0E8EJ1\alexs <alexshockency@gmail.com>
2021-02-24 06:21:19 -0700

    Trying dark theme see what you think https://bareynol.github.io/mui-theme-creator/ great website

--- 5e_init_tracker/src/App.js
+++ 5e_init_tracker/src/App.js
@@ -3,9 +3,33 @@ import { Link, Route, Switch } from "react-router-dom";
 import MonsterInfo from "./InitiativeTrackerApp/components/MonsterInfo";
 import SpellInfo from "./InitiativeTrackerApp/components/SpellInfo";
 import ConditionInfo from "./InitiativeTrackerApp/components/ConditionInfo";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#b53f3f',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  overrides: {
+    // // Style sheet name ⚛️
+    // MuiButton: {
+    //   // Name of the rule
+    //   text: {
+    //     // Some CSS
+    //     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    //   },
+    // },
+  },
+});
 
 export default function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div>
       <Route exact path="/">
         <AppDrawer />
@@ -17,5 +41,6 @@ export default function App() {
       <Route exact path="/condition/:id"  component={ConditionInfo}>
       </Route>
     </div>
+    </ThemeProvider>
   );
 }
